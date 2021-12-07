a5f536bf88e297fedce70f21dbb57e80b9451e56
Kiana Caston <kiana.caston@betterment.com>
2021-12-07 16:17:26 -0500

    day6 part2

--- 2021/day6/src/main.rs
+++ 2021/day6/src/main.rs
@@ -2,8 +2,13 @@ use std::io::{self, BufReader};
 use std::io::prelude::*;
 use std::fs::File;
 
-fn main() -> io::Result<()> {
-    let file = File::open("input.txt")?;
+fn main() {
+    // part_1();
+    part_2();
+}
+
+fn part_1() -> io::Result<()> {
+    let file = File::open("example_input.txt")?;
     let br = BufReader::new(file);
 
     let mut fish: Vec<_> = Vec::new();
@@ -16,7 +21,7 @@ fn main() -> io::Result<()> {
 
     let mut day = 0;
 
-    while day < 80 {
+    while day < 12 {
         let mut i = 0;
         let mut count = 0;
 
@@ -37,8 +42,49 @@ fn main() -> io::Result<()> {
         day = day + 1;
     }
 
-    // println!("fish: {:?}", fish);
-    println!("answer: {}", fish.len());
+    println!("answer part 1: {}", fish.len());
+
+    Ok(())
+}
+
+fn part_2() -> io::Result<()> {
+    let file = File::open("input.txt")?;
+    let br = BufReader::new(file);
+
+    let mut initial: Vec<usize> = Vec::new();
+
+    for line in br.lines() {
+        let l = line.unwrap();
+
+        initial = l.split(",").map(|x| x.parse().unwrap()).collect();
+    }
+
+    let mut day = 0;
+    let mut fish = vec![0; 9];
+
+    for f in initial {
+        fish[f] += 1;
+    }
+
+    while day < 256 {
+
+        let first = fish[0];
+
+        fish.rotate_left(1);
+
+        fish[8] = first;
+        fish[6] += first;
+
+        day += 1;
+    }
+
+    let mut count: u128 = 0;
+
+    for x in fish.clone() {
+        count += x;
+    }
+
+    println!("answer part 2: {}", count);
 
     Ok(())
 }
